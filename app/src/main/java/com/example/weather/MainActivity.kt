4efbf9af57ab813170361956e1633f0fc67a308a
simen <simen.ts@hotmail.com>
2023-02-27 09:56:02 +0100

    clean

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -39,7 +39,6 @@ class MainActivity : ComponentActivity() {
             val weather by weatherDetails.collectAsState()
 
             WeatherTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
